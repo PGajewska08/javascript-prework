c8e1d0dec818a336302117407931750864b72130
Paulina Gajewska <gajewska.paulina08@gmail.com>
2024-10-23 21:35:50 +0200

    Add style

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var ddd;
+ddd = document.getElementById('messages');
+ddd.innerHTML = 'Zaczynamy!';
 function printMessage(msg){
 	var div = document.createElement('div');
 	div.innerHTML = msg;
@@ -7,11 +10,18 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+var pointsPlayer, pointsComp, pointsPlayerBox, pointsCompBox;
+pointsPlayerBox = document.getElementById('points-player');
+pointsCompBox = document.getElementById('points-comp');
+pointsComp = 0;
+pointsPlayer = 0;
+function aktualizujWynik(){
+  pointsPlayerBox.innerHTML = pointsPlayer;
+  pointsCompBox.innerHTML = pointsComp;
+}
+aktualizujWynik();
+var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput, pointsComp, pointsPlayer;
 
-var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
-/**
- * Get move name
- */
 function getMoveName(argMoveId) {
   if (argMoveId == 1) {
     return 'kamień';
@@ -27,31 +37,35 @@ function getMoveName(argMoveId) {
     return 'kamień';
   }
 }
-/**
- * Display result
- */
+
 function displayResult(argPlayerMove, argComputerMove) {
   if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
     printMessage('Wygrywasz!');
+    pointsPlayer += 1;
+    aktualizujWynik();
   }
   else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
-	printMessage('Wygrywasz!');
+	  printMessage('Wygrywasz!');
+    pointsPlayer += 1;
+    aktualizujWynik();
   } 
   else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
-	printMessage('Wygrywasz!');
+	  printMessage('Wygrywasz!');
+    pointsPlayer += 1;
+    aktualizujWynik();
   } 
   else if (argPlayerMove == argComputerMove) {
 	printMessage('Remis!');
   } 
   else {
     printMessage('Przegrywasz :(');
+    pointsComp += 1;
+    aktualizujWynik();
   }
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+  console.log('Punkty komputera: '+pointsComp);
+  console.log('Punkty playera: '+ pointsPlayer);
 }
-
-////////////////////////////////////////////////////////////////
-//kod właściwy
-
 
 var buttonRock, buttonPaper, buttonScissors;
 
@@ -59,7 +73,13 @@ buttonRock = document.getElementById('button-rock');
 buttonPaper = document.getElementById('button-paper');
 buttonScissors = document.getElementById('button-scissors');
 
-
+function buttonClicked(argButtonName){
+  clearMessages();
+  playerMove = argButtonName;
+  randomNumber = Math.floor(Math.random() * 3 + 1);
+  computerMove = getMoveName(randomNumber);
+  displayResult(playerMove, computerMove);
+}
 
 buttonRock.addEventListener('click', function(){
     buttonClicked('kamień')
@@ -71,10 +91,4 @@ buttonScissors.addEventListener('click', function(){
     buttonClicked('nożyce')
 });
 
-function buttonClicked(argButtonName){
-  clearMessages();
-  playerMove = argButtonName;
-  randomNumber = Math.floor(Math.random() * 3 + 1);
-  computerMove = getMoveName(randomNumber);
-  displayResult(playerMove, computerMove);
-}
+
